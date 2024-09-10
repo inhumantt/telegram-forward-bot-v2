3b0e8a4d0efeea85f7944891588e83bfc72270b2
Inhuman <121882409+inhumantt@users.noreply.github.com>
2024-09-10 22:53:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from pyrogram import Client, filters
 logger = logging.getLogger(__name__)
 
 # Pyrogram setup
-api_id = "29930260"
-api_hash = "39ce391df95a43d32fd09dac7a4eabbf"
-phone_number = "+14025109303"
+api_id = "your_api_id" # Edit here
+api_hash = "your_api_hash" # Edit here
+phone_number = "telegram_number" # Edit here
 
 # Destination chats for ULP and logs
-ulp_destination_chat = -1001227119708  # Destination chat ID for ULP files
-logs_destination_chat = -10012345678  # Destination chat ID for log files (example)
+ulp_destination_chat = chat_id  # Destination chat ID for ULP files 
+logs_destination_chat = chat_d  # Destination chat ID for log files (example)
 
 # Chat IDs for file types
-ulp_file_chats = [-100032763]  # List of ULP chat IDs
-logs_file_chats = [-1007326373]  # List of log chat IDs
+ulp_file_chats = [chat_id]  # List of ULP chat IDs
+logs_file_chats = [chat_id]  # List of log chat IDs
 
-user_client = Client("my_account", api_id=api_id, api_hash=api_hash, phone_number=phone_number)
+user_client = Client("session_name", api_id=api_id, api_hash=api_hash, phone_number=phone_number) # Edit here
 
 @user_client.on_message(filters.chat(ulp_file_chats) & filters.document)
 async def forward_ulp_file(_, message):
